aeca6b9b51fd574eca7fb971923c04a8680d2694
Liu Yanbo <yanbo.liu@ucarinc.com>
2018-12-17 14:13:17 +0800

    update

--- ios_uicode_gen_web/config-overrides.js
+++ ios_uicode_gen_web/config-overrides.js
@@ -1,6 +1,7 @@
 const { injectBabelPlugin } = require('react-app-rewired')
 
 module.exports = function override (config, env) {
+  console.log('injectBabelPlugin')
   config = injectBabelPlugin(
     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
     config

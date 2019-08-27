31abdc30f40e396187b9cbc86675017e4261ddbc
Liu Yanbo <yanbo.liu@ucarinc.com>
2019-08-27 14:39:42 +0800

    add build

---
+++ ios_uicode_gen_web/build/precache-manifest.9e26c67948864ef4c75846c4416435a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3da0a8f7dc403d21ad88",
+    "url": "/static/js/main.3da0a8f7.chunk.js"
+  },
+  {
+    "revision": "589bdebbfd99101ef612",
+    "url": "/static/js/1.589bdebb.chunk.js"
+  },
+  {
+    "revision": "3da0a8f7dc403d21ad88",
+    "url": "/static/css/main.03e330c9.chunk.css"
+  },
+  {
+    "revision": "589bdebbfd99101ef612",
+    "url": "/static/css/1.236d0579.chunk.css"
+  },
+  {
+    "revision": "9f388ca5dd2318231aaafc57c667820a",
+    "url": "/index.html"
+  }
+];
